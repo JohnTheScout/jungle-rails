579482b551f2ef4c8621875f5bb84cd978eef885
John Goff <gygn.johngoff@gmail.com>
2017-09-05 12:47:02 -0400

    setup some testing data

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "Vistor navigates to home page", type: :feature, js: true do
+  #SETUP
+  before :each do
+    @cat = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @cat.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
   scenario 'they see all products' do 
+    #ACT
     visit root_path
+    #DEBUG
     save_screenshot
+    #VERIFY
+    expect(page).to have_css 'article.product'
   end
 end
