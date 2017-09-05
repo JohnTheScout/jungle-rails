78a0c0db40319e9e9bbf488491d5195b4e3decc9
John Goff <gygn.johngoff@gmail.com>
2017-09-05 15:02:07 -0400

    setup add to cart spec

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  #SETUP
+  before :each do
+    @cat = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @cat.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'the cart icon is updated' do
+    #ACT
+    visit root_path
+    click_on 'Add', match: :first
+    #DEBUG
+    save_screenshot
+    #VERIFY
+    puts page.html
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
